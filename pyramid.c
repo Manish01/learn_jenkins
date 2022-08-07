3009fe8e02fd416f1dca3ed0f6e1b9668fbaa57d
Manish01 <manicche.maxgood@gmail.com>
2022-08-07 19:04:47 +0530

    MP added c program

--- pyramid.c
+++ pyramid.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 int main() {
    int i, j, rows;
-#printf("Enter the number of rows: ");
-#   scanf("%d", &rows);
    for (i = 1; i <= 10; ++i) {
       for (j = 1; j <= i; ++j) {
          printf("* ");
